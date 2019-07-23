ce3f2ca38c818e666be0bb892fcac06a73133d49
Filipe do Ó Cavalcanti <37779557+fdcavalcanti@users.noreply.github.com>
2019-07-23 17:14:17 -0300

    Last version
    Decodes and saves the file.
    Last change before presentation at engineering school.

--- wsat-decode.py
+++ wsat-decode.py
@@ -1,27 +1,28 @@
 import scipy.io.wavfile as sio
 import scipy.signal as sig
-import matplotlib.pyplot as plt
+import scipy.ndimage as ndimg
 import numpy as np
-from PIL import Image
+import cv2
 
 #Import raw 11025 Hz wav file
-(Fs, audioRaw) = sio.read('DataAq/Resampled/N18_4827.wav')
+(Fs, audioRaw) = sio.read('DataAq/Resampled/20190523_195020Z_137100000Hz_AF.wav')
 audioRaw =  audioRaw/(2.**15)
 lineLen = int(np.floor(Fs/2))
 lineTotal = int(np.floor(len(audioRaw)/lineLen))
 audioRaw = audioRaw[0:lineTotal*lineLen]
-medFiltOn = 0
- 
+medFiltOn = 1
+
 #Filter audio with Hilbert transform and get absolute value
 audioFiltered = sig.hilbert(audioRaw)
 signal = np.abs(audioFiltered)
 #Optional
 if medFiltOn == 1:
-    signal = sig.medfilt(signal,7)
+    #signal = sig.medfilt(signal,5)
+    signal = ndimg.median_filter(signal,7)
 
 #Generate syncA signal
 T = 1/4160
-t = np.linspace(0,28*T,77,endpoint=True)
+t = np.linspace(0,28*T,77,endpoint=True)#77 -> 29*2.65
 sqr = 2*sig.square(2*np.pi*1040*t-4*T)
 corr = np.correlate(signal,sqr,'full')
 syncA = corr[len(sqr):len(corr)]
@@ -29,9 +30,7 @@ syncA = corr[len(sqr):len(corr)]
 #Map to grayscale
 maxVal = np.amax(signal)
 Ydig = np.floor(255*signal/maxVal)
-for i in range(len(Ydig)):
-    if Ydig[i] < 0:
-        Ydig[i] = 0
+Ydig[Ydig<0] = 0
 
 #Get max correlation points
 range1 = int(1); range2 = lineLen; matpos = []
@@ -48,5 +47,19 @@ for pos in matpos:
     newData = Ydig[pos:pos+lineLen]
     fig1 = np.vstack((fig1,newData))
 
-fig1 = np.array(fig1,dtype=int) #plt.imshow(fig1,cmap='gray')
-fig2 = Image.fromarray(fig1) #fig2.show()
+#fig1 = sig.medfilt2d(fig1,3) #3x3 mean
+
+fig1 = np.uint8(fig1) #plt.imshow(fig1,cmap='gray')
+fig2 = cv2.resize(np.float32(fig1),(2080,lineTotal)) #Fig1 com dimensão corrigida
+fig2 = np.uint8(fig2)
+#Plot
+#cv2.namedWindow('APT Fig', cv2.WINDOW_NORMAL)
+#cv2.resizeWindow('APT Fig', 1010, 600)
+cv2.imwrite("ITAescura.jpg",fig2)
+cv2.imshow("APT Fig", fig2)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+############################################################
+print('Sample frequency: ' + str(Fs) + ' Hz')
+print('Original resolution: ' + str(np.shape(fig1)))
